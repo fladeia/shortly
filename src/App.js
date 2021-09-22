c5945189448f21f0577f3b7070c6219a8e832aa5
fladeia <252842.5s@gmail.com>
2021-09-22 13:25:18 -0300

    added header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import logo from './assets/images/logo.svg'
 function Header() {
   return (
   <div className="App-header">
-
     <div className="header-container">
       <nav className="App-nav-desktop">
         <div className="logo-items">
           <img src={logo} alt="logo" className="logo" />
-            <ol className="nav-items">
+          <ol className="nav-items">
               <li>Features</li>
               <li>Pricing</li>
               <li>Resources</li>
@@ -20,20 +19,17 @@ function Header() {
           <button className="btn-sign-up" >Sign Up</button>
         </div>
       </nav>
-
       <div className="hero">
         <h1>More than just shorter links</h1>
-        <p>Build your brand’s recognition and get detailed insights 
+        <p>Build your brand’s recognition and get detailed <br /> insights 
         on how your links are performing.</p>
-        <button>Get Started</button>
+        <button className="btn-get-starter">Get Started</button>
       </div>
-
       <div className="shortIt">
         <input type="text" placeholder="Shorten a link here..." />
-        <button>Shorten It!</button>
+        <button className="btn-shorten-it">Shorten It!</button>
       </div>
     </div>
-
   </div>
   )
 }
@@ -43,23 +39,5 @@ export function App() {
     <>  
       <Header></Header>
     </>
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
