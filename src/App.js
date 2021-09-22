805dd98378222c870e11e36c5af657707b49a3c1
fladeia <252842.5s@gmail.com>
2021-09-22 09:28:39 -0300

    added nav css

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ function Header() {
   return (
   <div className="App-header">
 
-    <nav className="App-nav-desktop">
-      <div className="logo-items">
-        <img src={logo} alt="logo" className="logo" />
-          <ol className="nav-items">
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Resources</li>
-          </ol>
-      </div>
-      <div className="login-singUp">
-        <span className="login">Login</span>
-        <button>Sign Up</button>
-      </div>
-    </nav>
+    <div className="header-container">
+      <nav className="App-nav-desktop">
+        <div className="logo-items">
+          <img src={logo} alt="logo" className="logo" />
+            <ol className="nav-items">
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Resources</li>
+            </ol>
+        </div>
+        <div className="login-singUp">
+          <span className="login">Login</span>
+          <button className="btn-sign-up" >Sign Up</button>
+        </div>
+      </nav>
 
-    <div className="hero">
-      <h1>More than just shorter links</h1>
-      <p>Build your brand’s recognition and get detailed insights 
-      on how your links are performing.</p>
-      <button>Get Started</button>
-    </div>
+      <div className="hero">
+        <h1>More than just shorter links</h1>
+        <p>Build your brand’s recognition and get detailed insights 
+        on how your links are performing.</p>
+        <button>Get Started</button>
+      </div>
 
-    <div className="shortIt">
-      <input type="text" placeholder="Shorten a link here..." />
-      <button>Shorten It!</button>
+      <div className="shortIt">
+        <input type="text" placeholder="Shorten a link here..." />
+        <button>Shorten It!</button>
+      </div>
     </div>
 
   </div>
