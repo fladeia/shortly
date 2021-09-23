ac5c326f2077d2bf2de02bc849644f677cc048f8
fladeia <252842.5s@gmail.com>
2021-09-23 13:30:01 -0300

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+import './Footer.css'
+import logo from "../assets/images/logo.svg"
+import iconFacebook from "../assets/images/icon-facebook.svg"
+import iconTwitter from "../assets/images/icon-twitter.svg"
+import iconPinterest from "../assets/images/icon-pinterest.svg"
+import iconInstagram from "../assets/images/icon-instagram.svg"
+
+export function Footer() {
+    return (
+        <footer>
+            <div className="footer">
+                <img src={logo} className="logo"></img>
+                <div className="footer-nav">
+                    <div className="features">
+                        <ul>
+                            <li className="head">Features</li>
+                            <li>Link Shortening</li>
+                            <li>Branded Links</li>
+                            <li>Analitics</li>
+                        </ul>
+                    </div>
+                    <div className="resources">
+                        <ul>
+                            <li className="head">Resources</li>
+                            <li>Blog</li>
+                            <li>Developers</li>
+                            <li>Support</li>
+                        </ul>
+                    </div>
+                    <div className="company">
+                        <ul>
+                            <li className="head">Company</li>
+                            <li>About</li>
+                            <li>Our Team</li>
+                            <li>Careers</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                <div className="social-icons">
+                    <img src={iconFacebook}></img>
+                    <img src={iconTwitter}></img>
+                    <img src={iconPinterest}></img>
+                    <img src={iconInstagram}></img>
+                </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
