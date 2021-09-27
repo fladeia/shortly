a6df39bc217c9e543e63287f5690dde58665d781
fladeia <252842.5s@gmail.com>
2021-09-27 14:58:08 -0300

    fix shorten list

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,30 @@ import { ButtonSignUp, ButtonGetStarted } from './parts/Buttons'
 export function Header() {
 
   const searchUrl = useRef('')
-  const [ shortenItUrl, setShortenItUrl ] = useState('') 
+
+  const [ requiredUrl, setRequiredUrl ] = useState('https://www.google.com') 
+  const [ requeredUrlList, setrequeredUrlList ] = useState([])
+  const [ shortenList, setShortenList ] = useState([])
+
+  const fetchData = () => {
+    fetch(`https://api.shrtco.de/v2/shorten?url=${requiredUrl}`)
+    .then(res => res.json())
+    .then(data => {
+      setrequeredUrlList([...requeredUrlList, requiredUrl])
+      setShortenList([...shortenList, data.result.short_link2])
+      console.log(shortenList)
+      console.log(requeredUrlList)
+    })
+  }
 
   const clickHandler = (e) => {
     e.preventDefault()
-    setShortenItUrl(searchUrl.current.value)
-
-    fetch(`https://api.shrtco.de/v2/shorten?url=${shortenItUrl}`)
-    .then(res => res.json())
-    .then(data => console.log(data.result.code["short_link"]))
+    setRequiredUrl(searchUrl.current.value)
+    // console.log(requiredUrl);
+    fetchData()
   }
 
-    return (
+  return (
     <div className="App-header">
       <div className="header-container">
         <nav className="App-nav-desktop">
@@ -42,11 +54,14 @@ export function Header() {
         </div>
         <div className="shortIt">
           <form>
-            <label htmlFor="name">Url shorten it</label>
+            <label htmlFor="name"></label>
             <input type="text" id="name" ref={searchUrl} placeholder="Shorten a link here..." />
             <button className="btn-shorten-it" onClick={clickHandler}>Shorten It!</button>
           </form>
         </div>
+        <div className="shorten">{
+          shortenList.map(shorten => <h3>{shorten}</h3>)
+        }</div>
       </div>
     </div>
     )
